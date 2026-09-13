21ea5c4be4b8a4f5ce0ae8a2b7ab7ca10dcbc3fe
Aliya Thabasum <coolaliya07@gmail.com>
2026-09-13 21:53:05 +0530

    Create templates.py

---
+++ templates.py
@@ -0,0 +1,41 @@
+TEMPLATES = {
+    "python": """__pycache__/
+*.py[cod]
+.venv/
+venv/
+env/
+.env
+.vscode/
+.idea/
+*.log
+""",
+
+    "node": """node_modules/
+.env
+.env.*
+dist/
+build/
+coverage/
+.vscode/
+.idea/
+*.log
+""",
+
+    "java": """*.class
+target/
+out/
+*.jar
+.idea/
+.vscode/
+*.log
+""",
+
+    "web": """node_modules/
+dist/
+build/
+.env
+.vscode/
+.idea/
+*.log
+"""
+}
